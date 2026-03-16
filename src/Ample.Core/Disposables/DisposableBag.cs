eebf633b3369b52d6bc5782c46e01a06ad858ebf
GHCentre <ghcentre@gmail.com>
2026-03-16 09:49:39 +0300

    Add DisposableBag, DisposableBag<T>

---
+++ src/Ample.Core/Disposables/DisposableBag.cs
@@ -0,0 +1,129 @@
+﻿using Ample.Core.GuardClauses;
+
+namespace Ample.Core.Disposables;
+
+/// <summary>
+/// Contains extension methods for the <see cref="DisposableBag{T}"/>.
+/// </summary>
+public static class DisposableBag
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DisposableBag{T}"/> with no assigned value.
+    /// </summary>
+    /// <returns><see cref="DisposableBag{T}"/> with value assigned to <see langword="null"/>.</returns>
+    public static DisposableBag<object> Create()
+    {
+        return new DisposableBag<object>(null);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DisposableBag{T}"/> with the value specified in the parameter.
+    /// </summary>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    /// <param name="value">The value to store in the bag.</param>
+    /// <returns><see cref="DisposableBag{T}"/>.</returns>
+    public static DisposableBag<T> Create<T>(T value) where T : class
+    {
+        return new DisposableBag<T>(value);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DisposableBag{T}"/> with the value specified in the parameter,
+    /// and adds the value to the collection of disposables which are disposed when the bag is disposed.
+    /// </summary>
+    /// <typeparam name="T">The type of the value. The type must implement the <see cref="IDisposable"/> interface.</typeparam>
+    /// <param name="value">The value to store in the bag.</param>
+    /// <returns><see cref="DisposableBag{T}"/>.</returns>
+    public static DisposableBag<T> CreateForDisposable<T>(T value) where T : class, IDisposable
+    {
+        Guard.Against.Null(value);
+        var bag = new DisposableBag<T>(value);
+        return bag.With(value);
+    }
+
+    /// <summary>
+    /// Adds the disposable object to the collection of objects which are disposed when the bag is disposed.
+    /// </summary>
+    /// <typeparam name="T">The type of the value stored in the bag.</typeparam>
+    /// <param name="bag">The bag.</param>
+    /// <param name="disposable">The disposable object.</param>
+    /// <returns>The bag.</returns>
+    /// <exception cref="ArgumentNullException">The bag is <see langword="null"/>,
+    /// or the disposable object is <see langword="null"/>.</exception>
+    public static DisposableBag<T> With<T>(this DisposableBag<T> bag, IDisposable disposable) where T : class
+    {
+        Guard.Against.Null(bag);
+        Guard.Against.Null(disposable);
+
+        bag.Add(disposable.Dispose);
+
+        return bag;
+    }
+
+    /// <summary>
+    /// Adds the action to the collection of actions which are executed when the bag is disposed.
+    /// </summary>
+    /// <typeparam name="T">The type of the value stored in the bag.</typeparam>
+    /// <param name="bag">The bag.</param>
+    /// <param name="action">The action</param>
+    /// <returns>The bag.</returns>
+    /// <exception cref="ArgumentNullException">The bag is <see langword="null"/>,
+    /// or the action is <see langword="null"/>.</exception>
+    public static DisposableBag<T> With<T>(this DisposableBag<T> bag, Action action) where T : class
+    {
+        Guard.Against.Null(bag);
+        Guard.Against.Null(action);
+
+        bag.Add(action);
+
+        return bag;
+    }
+
+    /// <summary>
+    /// Contains the extension method for the <see cref="DisposableBag{T}"/> class.
+    /// </summary>
+    /// <typeparam name="T">The type of the value stored in the bag.</typeparam>
+    /// <param name="bag">The <see cref="DisposableBag{T}"/> bag.</param>
+    extension<T>(DisposableBag<T> bag) where T : class
+    {
+        /// <summary>
+        /// Adds the collection of disposables to the bag. Disposables are disposed when the bag is disposed.
+        /// </summary>
+        /// <param name="disposables">Sequence of disposables.</param>
+        /// <returns>The bag.</returns>
+        /// <exception cref="ArgumentNullException">The bag is <see langword="null"/>,
+        /// or the sequence of disposables is <see langword="null"/>.</exception>
+        public DisposableBag<T> With(params IEnumerable<IDisposable> disposables)
+        {
+            Guard.Against.Null(bag);
+            Guard.Against.Null(disposables);
+
+            foreach (var disposable in disposables)
+            {
+                bag.With(disposable); // will check for null
+            }
+
+            return bag;
+        }
+
+        /// <summary>
+        /// Adds the collection of actions to the bag. Actions are executed when the bag is disposed.
+        /// </summary>
+        /// <param name="actions">The sequence of actions.</param>
+        /// <returns>The bag.</returns>
+        /// <exception cref="ArgumentNullException">The bag is <see langword="null"/>,
+        /// or the sequence of actions is <see langword="null"/>.</exception>
+        public DisposableBag<T> With(params IEnumerable<Action> actions)
+        {
+            Guard.Against.Null(bag);
+            Guard.Against.Null(actions);
+
+            foreach (var action in actions)
+            {
+                bag.With(action); // will check for null
+            }
+
+            return bag;
+        }
+    }
+}
---
+++ src/Ample.Core/Disposables/DisposableBagOfT.cs
@@ -0,0 +1,106 @@
+﻿using Ample.Core.GuardClauses;
+
+namespace Ample.Core.Disposables;
+
+/// <summary>
+/// Provides a container for an arbitrary value and a collection of <see cref="Action"/>s.
+/// When the container is disposed, all actions are executed.
+/// </summary>
+/// <typeparam name="T">The type of the object.</typeparam>
+/// <seealso cref="DisposableBag"/>
+/// <remarks>
+/// <para>Use the <see cref="Add(Action)"/> method to add an action to the collection of actions
+/// which are executed when the container is disposed.</para>
+/// <para>Last action added is executed first.</para>
+/// <para>To dispose the object on bag's disposal add the object's <c>Dispose</c> method:
+/// <code>bag.Add(disposable.Dispose);</code>,
+/// or use the <see cref="DisposableBag.With{T}(DisposableBag{T}, IDisposable)"/>
+/// extension method.
+/// </para>
+/// </remarks>
+/// <example>
+/// <code><![CDATA[
+/// private SomeClass CreateWithDependencies()
+/// {
+///     var someDependency = new SomeDependency();
+///     var anotherDependency = new AnotherDependency(someDependency);
+///     var someClass = new SomeClass(anotherDependency);
+///     var bag = DisposableBag.Create(someClass).With(someDependency, anotherDependency);
+///     return bag;
+/// }
+/// 
+/// using (var someClassBag = _someClassBagFactory())
+/// {
+///     var someClass = someClassBag.Value;
+///     // do something with someClass
+/// 
+///     // disposables added to the bag are disposed in the reverse order:
+///     // anotherDependency.Dispose()
+///     // someDependency.Dispose()
+/// }
+/// ]]></code>
+/// </example>
+/// <remarks>
+/// Initializes a new instance of the <see cref="DisposableBag{T}"/> class.
+/// </remarks>
+/// <param name="value">The value to store in the bag.</param>
+public class DisposableBag<T>(T? value) where T : class
+{
+    private readonly Stack<Action> _actions = new();
+
+    /// <summary>
+    /// Adds a <see cref="Action"/> to the collection of actions which are executed when the container is disposed.
+    /// </summary>
+    /// <param name="action">The action to add.</param>
+    public virtual void Add(Action action)
+    {
+        Guard.Against.Null(action);
+
+        ObjectDisposedException.ThrowIf(_disposed, this);
+        _actions.Push(action);
+    }
+
+
+    /// <summary>
+    /// Gets the value stored in the bag.
+    /// </summary>
+    /// <value>The value stored in the bag.</value>
+    public T? Value { get; } = value;
+
+
+    #region IDisposable
+
+    private bool _disposed = false;
+
+    /// <summary>
+    /// Performs the object cleanup.
+    /// </summary>
+    /// <param name="disposing"><see langword="true"/> if disposing managed resources,
+    /// or <see langword="false"/> otherwise.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        _disposed = true;
+
+        if (disposing)
+        {
+            foreach (var action in _actions)
+            {
+                action();
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    #endregion
+}
